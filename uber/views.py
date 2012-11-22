77d0340bb01b8dd71eff5aecb74d44c0de792d31
Zack Dever <zackdever@gmail.com>
2012-11-22 02:30:36 -0600

    made decorator for checking content-type

--- uber/views.py
+++ uber/views.py
@@ -1,17 +1,27 @@
 from bson.objectid import ObjectId
+from functools import wraps
 from flask import request, jsonify, url_for, Response
 from uber import app
 from uber.models import Location
+
+def check_content_type(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        if request.method == 'POST' or request.method == 'PUT':
+            ctype = request.headers['Content-Type']
+            if ctype != 'application/json':
+                return bad_request("Expected 'application/json', got '%s'" % ctype)
+        return f(*args, **kwargs)
+    return decorated
 
 @app.route('/')
 def index():
     return 'you see a map.\n'
 
 @app.route('/locations/', methods = ['GET', 'POST'])
+@check_content_type
 def locations():
     if request.method == 'POST':
-        if not is_content_json(): return require_json()
-
         location = Location.parse_json(request.json, has_id=False)
         if not location: return bad_request()
 
@@ -30,6 +40,7 @@ def locations():
 
 @app.errorhandler(404)
 @app.route('/locations/<id>', methods = ['GET', 'PUT', 'DELETE'])
+@check_content_type
 def location(id):
     if not ObjectId.is_valid(id):
         return bad_request("'id' is either missing or is of the wrong type")
@@ -40,8 +51,6 @@ def location(id):
     if not location: return not_found()
 
     if request.method == 'PUT':
-        if not is_content_json(): return require_json()
-
         updated = Location.parse_json(request.json, has_id=True)
         if not updated or updated['_id'] != oid: return bad_request()
 
@@ -93,10 +102,3 @@ def server_error(message=None):
 
     resp.status_code = 404
     return resp
-
-def is_content_json():
-    return request.headers['Content-Type'] == 'application/json'
-
-def require_json():
-    return bad_request("Expected 'application/json', got '%s'" %
-        request.headers['Content-Type'])
